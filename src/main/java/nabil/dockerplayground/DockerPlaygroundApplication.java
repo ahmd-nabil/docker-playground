050849db36e6b0c6346c31dba48e3a1ab0c9442d
Ahmed Nabil <ahmednabil@datagearbi.com>
2023-10-11 16:56:23 +0300

    first commit

---
+++ src/main/java/nabil/dockerplayground/DockerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package nabil.dockerplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPlaygroundApplication.class, args);
+    }
+
+}
